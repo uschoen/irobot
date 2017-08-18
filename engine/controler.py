25b643f151a930d643dccea869e944ba9bf21f38
uschoen <uschoen@192.168.3.24>
2017-08-18 21:05:37 +0200

    adding controller class

---
+++ engine/controler.py
@@ -0,0 +1,17 @@
+'''
+Created on 18.08.2017
+
+@author: uschoen
+'''
+
+class controller(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
